7486864abb78c616b2472c43d6ca162eeb12b02c
Betulcan7 <9019870@student.zadkine.nl>
2023-01-31 14:03:04 +0100

    home

--- home.php
+++ home.php
@@ -8,9 +8,9 @@
 </head>
 <body>
     <ul>
-        <li><a href="home.html">home</a></li>
-        <li><a href="TestPagTuncay.html">Tuncay</a></li>
-        <li><a href="TestPagEmre.html">Emre</a></li>
+        <li><a href="home.php">home</a></li>
+        <li><a href="TestPagTuncay.php">Tuncay</a></li>
+        <li><a href="TestPagEmre.php">Emre</a></li>
         <li><a href="testbetul.php">Betul</a></li>
     </ul>
 
