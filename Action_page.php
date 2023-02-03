f9ea0f9854d58c8f67c5eadfc23d3e5d5e729f11
EmreHBas <112864371+EmreHBas@users.noreply.github.com>
2023-02-03 10:24:12 +0100

    9.0

--- Action_page.php
+++ Action_page.php
@@ -3,4 +3,13 @@
 echo "Datum: " . date("Y/m/d") . "<br>";
 echo "Username: " . $_POST ["username"] . "<br>";
 echo "Password: " . $_POST ["password"];
+
+$len = strlen($_POST["username"]);
+echo "De lengte van username: $len";
+
+if ($len <5 ) {
+    echo "Username moet groter zijn dan 4 tekens <br>"; 
+} 
+
 ?>
+
