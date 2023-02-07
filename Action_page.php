81bf8b6d67f0d25a80aaabdfc5cd3e81efeb8ee3
TuncaySteinmann <112862509+TuncaySteinmann@users.noreply.github.com>
2023-02-07 13:37:06 +0100

    ja

--- Action_page.php
+++ Action_page.php
@@ -31,9 +31,10 @@ $a[0]= "jan";
 $a[1]= "rob";
 $a[2]= "piet";
 
+$b = array(10,11,12,34,100,1000);
 
 echo '<table border="1" width="200">';
-foreach($a as $value){
+foreach($b2 as $value){
 
     //toevoegen tr en td
    
