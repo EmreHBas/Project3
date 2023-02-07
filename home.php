3c89389137d1a058a8ca85d9c3decede94985007
Fufulele <112864040+Fufulele@users.noreply.github.com>
2023-02-07 13:08:06 +0100

    azad..

--- home.php
+++ home.php
@@ -12,7 +12,7 @@
         <li><a href="TestPagTuncay.php">Tuncay</a></li>
         <li><a href="TestPagEmre.php">Emre</a></li>
         <li><a href="Testbetul.php">Betul</a></li>
-        <li><a href="TestPageAzad.php">Azad</li>
+        <li><a href="TestPagAzad.php">Azad</li>
     </ul>
 
     <?php
