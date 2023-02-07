5437e2e811f5cf1c0d8a3e3adffa1ef8288eadf9
EmreHBas <112864371+EmreHBas@users.noreply.github.com>
2023-02-07 13:48:14 +0100

    $_POST

--- Action_page.php
+++ Action_page.php
@@ -24,6 +24,7 @@ if ($len <5 ) {
     echo "Username moet groter zijn dan 4 tekens <br>"; 
 }
 
+var_dump($_POST);
 
 
 /*array vullen*/ 
@@ -34,7 +35,7 @@ $a[2]= "piet";
 //$b = array(10,11,12,34,100,1000); //ook mogelijk!!
 
 echo '<table border="1" width= "200">';
-foreach($a as $value){
+foreach($_POST as $value){
 
     //toevoegen tr en td
     echo "<tr><td>";
