1756a7558d6c4ec715583b6db9aaeaf2b3cc40b5
EmreHBas <112864371+EmreHBas@users.noreply.github.com>
2023-02-14 13:04:14 +0100

    main_bieren 1.0

---
+++ main_bieren.php
@@ -0,0 +1,15 @@
+<?php
+//functie: Programme overzicht bieren
+// auteur: 
+
+//initialisatie
+include 'functions.php';
+
+//main
+
+//connect database bieren
+ConnectDb();
+
+//print bieren
+OvzBieren();
+
