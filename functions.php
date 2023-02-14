1bfe48ab2f106522907fcee723a59d2d7b0c6238
EmreHBas <112864371+EmreHBas@users.noreply.github.com>
2023-02-14 13:57:19 +0100

    123465

--- functions.php
+++ functions.php
@@ -10,7 +10,7 @@ function ConnectDb(){
     $dbname ="bieren"; 
 
 try {
-  $conn = new PDO("mysql:host=$servername;dbname= $dbname", $username, $password);
+  $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   echo "Connected successfully";
@@ -22,8 +22,6 @@ catch(PDOException $e) {
 return $conn;
 }
 
-function Ovzbieren()
-    echo "overzicht<br>";
 
 function Ovzbieren($getal){
     echo "overzicht<br>";
