c469ff9e971322392326fc0bad39a028aa52974c
EmreHBas <112864371+EmreHBas@users.noreply.github.com>
2023-02-14 13:45:44 +0100

    ?

--- functions.php
+++ functions.php
@@ -4,7 +4,24 @@
 
 function ConnectDb(){
     echo "connect<br>";
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname ="bieren"; 
+
+try {
+  $conn = new PDO("mysql:host=$servername;dbname= $dbname", $username, $password);
+  // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  echo "Connected successfully";
+} 
+catch(PDOException $e) {
+  echo "Connection failed: " . $e->getMessage();
 }
+}
+
+function Ovzbieren()
+    echo "overzicht<br>";
 
 function Ovzbieren($getal){
     echo "overzicht<br>";
