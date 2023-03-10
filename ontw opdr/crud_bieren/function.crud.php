def2ab98efef01d140934bf6c4ee0917c91c31e0
EmreHBas <112864371+EmreHBas@users.noreply.github.com>
2023-03-10 09:58:41 +0100

    1.crud

--- ontw opdr/crud_bieren/function.crud.php
+++ ontw opdr/crud_bieren/function.crud.php
@@ -9,4 +9,19 @@ function CrudBieren(){
     PrintCrudBier($result);
 }
 
+function PrintCrudBier($result){
+//zet de hele tabel in en variabele en print hem 1 keer
+$tabel = "<table border = 1px>";
+
+//Print header table
+
+//haal de kolommen uit de eerste [0] van het array
+$headers = array_keys($result[0]);
+$tabel .= "<tr>";
+foreach($headers as $headers){
+    $table .="<th bgcolor=gray>" .$header ."</th>"
+}
+
+}
+
 ?>
